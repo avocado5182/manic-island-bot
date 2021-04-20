4e1e3a562832da25a059673da69c047874a316c1
avocado5182 <haxozaposa123@gmail.com>
2021-04-19 23:30:27 -0400

    server info command/ts support pog

--- commands/serverinfo.ts
+++ commands/serverinfo.ts
@@ -1,15 +1,15 @@
-const { MessageEmbed } = require("discord.js");
+import { EmbedFieldData, Message, MessageEmbed } from "discord.js";
 
 module.exports = {
     name: "serverinfo",
     description: "Gets info about current guild.",
     aliases: [],
     debug: false,
-    execute(message, args) {
-        message.channel.send("**Info for " + message.guild.name + "**");
-        
-        let serverInfo;
+    execute(message: Message, args: String[]) {
+        message.channel.send(`**Info for ${message.guild.name}**`);
+        let serverInfo: EmbedFieldData[];
 
+        
         serverInfo.push({
             name: "Creation Date",
             value: message.guild.createdAt.getDate
@@ -19,20 +19,20 @@ module.exports = {
             name: "Owner",
             value: message.guild.owner.user.tag
         });
-
+            
         serverInfo.push({
             name: "Members",
             value: message.guild.memberCount
         });
 
-        const infoEmbed = new MessageEmbed()
+        const infoEmbed: MessageEmbed = new MessageEmbed()
             .setColor('#00ff99')
             .setTitle(message.guild.name)
             .setThumbnail("https://cdn.discordapp.com/emojis/779828495932981279.gif?v=1")
-            .setDescription("Info about " + message.guild.name)
+            .setDescription(`Info about ${message.guild.name}`)
             .addFields(serverInfo)
             .setFooter('Made with ❤️ by avocado#5277');
 
         message.channel.send(infoEmbed, { split: true });
     }
-};
+}
