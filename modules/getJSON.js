eb1b7cc98b94ef013f3134b2f15c35b95c4c23d8
avocado5182 <haxozaposa123@gmail.com>
2021-04-24 03:31:47 -0400

    added getkey/setkey for stats stuff

--- modules/getJSON.js
+++ modules/getJSON.js
@@ -1,16 +1,37 @@
 const fs = require('fs');
 
 module.exports = {
-    getBalance(gid, uid) {
+    getKey(gid, key) {
         const JSONpath = `./db/economy/${gid}.json`;
         try {
             if (fs.existsSync(JSONpath)) {
                 let JSONobj = JSON.parse(fs.readFileSync(JSONpath));
-                if (JSONobj.stats === null) {
-                    throw Error(`No balance for given user id! (${uid})`);
+                if (JSONobj[key] === null || JSONobj[key] === undefined) {
+                    throw Error(`No value found for given key! (${key}, ${gid})`);
                 } else {
-                    const user = JSONobj.stats.find(u => u.user === uid);
-                    return user.balance;
+                    return JSONobj[key];
+                }
+            } else {
+                throw Error(`No file for given guild id! (${gid})`);
+            }
+        } catch {
+            console.trace(err);
+        }
+    },
+
+    setKey(gid,key,val) {
+        const JSONpath = `./db/economy/${gid}.json`;
+        try {
+            if (fs.existsSync(JSONpath)) {
+                let JSONobj = JSON.parse(fs.readFileSync(JSONpath));
+                if (
+                (JSONobj[key] === null || JSONobj[key] === undefined) ||
+                (val === null || val === undefined)) {
+                    throw Error(`No value found for given key or value is null/undefined! (${key}, ${value} ${gid})`);
+                } else {
+                    JSONobj[key] = val;
+                    fs.writeFileSync(JSONpath, JSON.stringify(JSONobj));
+                    return JSONobj[key];
                 }
             } else {
                 throw Error(`No file for given guild id! (${gid})`);
@@ -20,54 +41,103 @@ module.exports = {
         }
     },
 
-    setBalance(gid, uid, bal) {
-        const JSONpath = `./db/economy/${gid}.json`;
-        try {
-            if (fs.existsSync(JSONpath)) {
-                let JSONobj = JSON.parse(fs.readFileSync(JSONpath));
-                if (JSONobj.stats === null) {
-                    const balances = [{
-                        user: uid,
-                        balance: bal
-                    }];
-
-                    JSONobj.stats = balances;
-                    fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
-                    return bal;
-                } else {
-                    let userIdx =
-                        JSONobj.stats.findIndex(u => u.user === uid);
-                        let user = JSONobj.stats[userIdx];
-                    if (!user) {
-                        // user doesnt exist but other users have balances
-                        let balance = {
-                            user: uid,
-                            balance: bal
-                        };
-
-                        JSONobj.stats.push(balance);
-                        fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
-                        return bal;
-                    } else {
-                        // user does exist and other users have balances
-                        JSONobj.stats[userIdx].balance = bal;
-                        fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
-                        return JSONobj.stats[userIdx].balance || bal;
-                    }
-                }
-            } else {
-                throw Error("No file for given guild id!");
-            }
-        } catch (err) {
-            console.trace(err);
+    getBalance(gid, uid) {
+        const statsArr = this.getKey(gid, "stats");
+        let userStats = statsArr.find(u => u.user === uid);
+        if (userStats === undefined) {
+            userStats = {
+                user: uid,
+                balance: 0
+            };
+            this.setBalance(gid,uid,0);
+            return 0;
         }
+        return userStats.balance;
+
+        // #region old
+        // const JSONpath = `./db/economy/${gid}.json`;
+        // try {
+        //     if (fs.existsSync(JSONpath)) {
+        //         let JSONobj = JSON.parse(fs.readFileSync(JSONpath));
+        //         if (JSONobj.stats === null) {
+        //             throw Error(`No balance for given user id! (${uid})`);
+        //         } else {
+        //             const user = JSONobj.stats.find(u => u.user === uid);
+        //             return user.balance;
+        //         }
+        //     } else {
+        //         throw Error(`No file for given guild id! (${gid})`);
+        //     }
+        // } catch(err) {
+        //     console.trace(err);
+        // }
+        // #endregion
+    },
+
+    setBalance(gid, uid, bal) {
+        // #region old
+        // const JSONpath = `./db/economy/${gid}.json`;
+        // try {
+        //     if (fs.existsSync(JSONpath)) {
+        //         let JSONobj = JSON.parse(fs.readFileSync(JSONpath));
+        //         if (JSONobj.stats === null) {
+        //             const balances = [{
+        //                 user: uid,
+        //                 balance: bal
+        //             }];
+
+        //             JSONobj.stats = balances;
+        //             fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
+        //             return bal;
+        //         } else {
+        //             let userIdx =
+        //                 JSONobj.stats.findIndex(u => u.user === uid);
+        //                 let user = JSONobj.stats[userIdx];
+        //             if (!user) {
+        //                 // user doesnt exist but other users have balances
+        //                 let balance = {
+        //                     user: uid,
+        //                     balance: bal
+        //                 };
+
+        //                 JSONobj.stats.push(balance);
+        //                 fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
+        //                 return bal;
+        //             } else {
+        //                 // user does exist and other users have balances
+        //                 JSONobj.stats[userIdx].balance = bal;
+        //                 fs.writeFileSync(JSONpath,JSON.stringify(JSONobj));
+        //                 return JSONobj.stats[userIdx].balance || bal;
+        //             }
+        //         }
+        //     } else {
+        //         throw Error("No file for given guild id!");
+        //     }
+        // } catch (err) {
+        //     console.trace(err);
+        // }
+        // #endregion
+        const statsArr = this.getKey(gid, "stats");
+        let userStats = statsArr.find(u => u.user === uid);
+        if (userStats === undefined) {
+            userStats = {
+                user: uid
+            };
+        }
+        userStats.balance = bal;
+        this.setKey(gid, "stats", statsArr);
+        return userStats.balance;
     },
     
     addBalance(gid, uid, bal) {
         const currBal = this.getBalance(gid, uid);
         return this.setBalance(gid, uid, currBal + bal);
+        // #region old II
 
-        // #region old
+        // const currBal = this.getBalance(gid, uid);
+        // return this.setBalance(gid, uid, currBal + bal);
+
+        // // #region old
             // const JSONpath = `./db/economy/${gid}.json`;
             // try {
             //     if (fs.existsSync(JSONpath)) {
