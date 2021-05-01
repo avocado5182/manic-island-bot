506502df33354505fa9134b1a57f5090fec8108f
avocado5182 <haxozaposa123@gmail.com>
2021-04-30 23:01:34 -0400

    fixed reload subfolder support

--- commands/debug/reload.js
+++ commands/debug/reload.js
@@ -1,23 +1,5 @@
 const fs = require("fs");
-
-function walk(dir) {
-    let results = [];
-    const list = fs.readdirSync(dir);
-    list.forEach(function(file) {
-        file = dir + '/' + file;
-        const stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) { 
-            /* Recurse into a subdirectory */
-            results = results.concat(walk(file));
-        } else { 
-            /* Is a file */
-            file_type = file.split(".").pop();
-            file_name = file.split(/(\\|\/)/g).pop();
-            if (file_type == "js") results.push(file);
-        }
-    });
-    return results;
-}
+const path = require("path");
 
 module.exports = {
 	name: 'reload',
@@ -27,14 +9,11 @@ module.exports = {
     usage: '<command name>',
 	execute(message, args) {
         const commandName = args[0].toLowerCase();
-        const command = message.client.commands.get(commandName)
-            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
-            || undefined;
-      
+        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName)) || undefined;
+
         if (command != undefined) {
             // delete require.cache[require.resolve(`./${command.name}.js`)];
-            // delete require.cache[require.resolve(`././${command.category??""}/${command.name}`)];
-            delete require.cache[require.resolve("./reload.js")];
+            delete require.cache[require.resolve(path.join(__dirname.substring(0, __dirname.length - 5), command.category ?? "", `${command.name}.js`))];
 
             try {
                 const newCommand = require(`../${command.category??""}/${command.name}`);
