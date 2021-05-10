dd4d5e650d3007d88ba7758d969303bb9b12ba3e
avocado5182 <haxozaposa123@gmail.com>
2021-05-10 15:01:21 -0400

    random messages (for index.js)

---
+++ messages.js
@@ -0,0 +1,19 @@
+module.exports = {
+    messages: [
+        ['PLAYING', 'with custom presences'],
+        ['PLAYING', 'bruh moment'],
+        ['PLAYING', 'flame is a catboy :3'],
+        ['PLAYING', 'with avocado'],
+        ['PLAYING', 'Catboy Fwame Simulator OwO'],
+        ['PLAYING',  'Playing Playing Playing Playing Playing Playing']
+        ['LISTENING', 'my thoughts'],
+        ['LISTENING', 'm!help'],
+        ['LISTENING', 'm!help'],
+        ['LISTENING', 'm!help'],
+        ['LISTENING', 'm!help'],
+        ['LISTENING', 'm!help'],
+        ['LISTENING', 'm!help'],
+        ['WATCHING', 'for the new feature in 5 hours'],
+        ['WATCHING', 'avocado dies??? (not CLICKBAIT) (gone wrong)']
+    ]
+}
