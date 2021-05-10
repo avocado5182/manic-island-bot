8f0908a47cd86ad66b89540ae4ba74315d62669b
avocado5182 <haxozaposa123@gmail.com>
2021-05-10 15:04:22 -0400

    fixed pog command

--- commands/fun/pog.js
+++ commands/fun/pog.js
@@ -6,13 +6,14 @@ module.exports = {
     category: "fun",
     cooldown: 5,
 	execute(message, args) {
+        if (args.length != 0) return;
         message.client.guilds.fetch('767327205646532648')
         .then(g => {
-            const pogmoji = g.emojis.cache.get((args[0] != null) ? args[0] : '767327205646532648');
+            const pogmoji = g.emojis.cache.get((args[0] != null) ? args[0] : '839856407260299314');
             message.channel.send(`${pogmoji.toString()} poggers!!! ${pogmoji.toString()}`);
         })
         .catch(err => {
-            message.channel.send("<@767327205646532648>");
+            message.channel.send("<:pog:839856407260299314> pog! <:pog:839856407260299314>");
         });
 	},
 };
