3c255831fc9ccf5c147483f684b3e81220c4213e
avocado5182 <haxozaposa123@gmail.com>
2021-05-14 22:34:09 -0400

    changed to use getJSON module :D

--- commands/economy/buyproduct.js
+++ commands/economy/buyproduct.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const { MessageEmbed } = require('discord.js');
-
+const { MessageEmbed, Guild } = require('discord.js');
+const guildJSON = require("../../modules/getJSON");
+const { trace } = require('console');
 
 module.exports = {
     name: "buyproduct",
@@ -11,49 +12,29 @@ module.exports = {
     usage: "<product name>",
     category: "economy",
     execute(message, args) {
-        const JSONpath = `./db/economy/${message.guild.id}.json`;
-        let JSONobj = {};
+        try {
+            let products = guildJSON.getKey(message.guild.id, "products");
+            const givenProductName = args.join(" ");
+            
+            const product = products.find(p => p.name === givenProductName);
+            if (product !== null || product !== undefined) {
+                const stats = guildJSON.getKey(message.guild.id, "stats");
+                let userStats = stats.find(u => u.user === message.author.id);
 
-        if (fs.existsSync(JSONpath)) {
-            JSONobj = JSON.parse(fs.readFileSync(JSONpath));
-
-            if (JSONobj.stats === null) {
-                return message.channel.send("You cannot buy anything since you have no currency. Please get some currency and try again.");
-            } else if (JSONobj.products === null) {
-                return message.channel.send("You cannot buy anything since there are no products. Please create a product and try again.");
-            } else {
-                // Both balances and products exist
-
-                // Product check
-                let givenProductName = args.join(" ");
-                let product = JSONobj.products.find(p => p.name === givenProductName);
-                if (product != null || product != undefined) {
-                    // Product found with given name
-                    let userIndex = JSONobj.stats.findIndex(b => b.user === message.author.id);
-                    let user = JSONobj.stats[userIndex];
-                    if (user !== null) {
-                        // Found balance too
-                        if (product.price <= user.balance) {
-                            // Can afford
-                            JSONobj.stats[userIndex].balance -= product.price;
-                            fs.writeFileSync(JSONpath, JSON.stringify(JSONobj));
-                            return message.reply(`Product *${product.name}* purchased for ${product.price} currency! You now have ${JSONobj.stats[userIndex].balance} currency.`);
-                        } else {
-                            // Cannot afford
-                            return message.channel.send("You cannot afford the specified product. Please try again.");
-                        }
-                    } else {
-                        // User has no balance object
-                        return message.channel.send("You have not gotten any currency yet. Please get some and try again.");
-                    }
-                } else {
-                    // Product doesn't exist
-                    return message.channel.send("The specified product does not exist. Are you mispelling the name of it? Please try again. Note that all product names are case sensitive.");
+                let balance = userStats.balance ?? 0;
+                if (balance >= product.price) {
+                    // user can buy the product
+                    balance -= product.price;
+                    userStats.balance = balance;
+                    return message.reply(`Product *${product.name}* purchased for ${product.price} currency! You now have ${balance} currency.`);
                 }
+            } else {
+                return message.channel.send("Your given product is invalid. Please try again.");
             }
-        } else {
-            // No <GUILDID>.json file
-            return message.channel.send(`<@${message.author.id}> It seems like you have not started using the economy commands. Please create a product, get some currency, and try again.`);
+        } catch(err) {
+            // error w/ no products
+            console.trace(err);
+            return message.channel.send("There are no products to buy. Please try again.");
         }
     }
 }
